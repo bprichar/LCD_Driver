8a3f71e4338dcefce0420d53f64c4178c3940b74
Brad Richardson <brad.richardson@transware.net>
2015-12-10 07:57:57 -0600

    Make the light flash also

--- src/FooProject/FooProject.cpp
+++ src/FooProject/FooProject.cpp
@@ -4,19 +4,23 @@
 
 FooClass FooObject;
 
+int ledPin = 13;
+
 void setup() {
 
 	Serial.begin(115200);
 	delay(1000);
-
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
 
 	Serial.println("Hello world");
 	FooObject.firstFooMethod();
+    digitalWrite(ledPin, HIGH);
 	delay(1000);
 	FooObject.secondFooMethod();
+    digitalWrite(ledPin, LOW);
 	delay(1000);
 
 }
