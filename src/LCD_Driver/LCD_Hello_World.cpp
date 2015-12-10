ca0a777f16c5978bafbc0c9a3f83473581ab124b
Brad Richardson <brad.richardson@transware.net>
2015-12-10 12:58:54 -0600

    Create a Hello World to test the LCD Driver

---
+++ src/LCD_Driver/LCD_Hello_World.cpp
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 4, 5, 6, 0, 1, 2, 3, 7, POSITIVE);
+int counter=0;
+
+void setup ()
+{
+    lcd.begin(16,2);
+    lcd.backlight();
+    for (int i=1; i <= 3; i++)
+    {
+        lcd.noBacklight();
+        delay(500);
+        lcd.backlight();
+        delay(500);
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello World!");
+}
+
+void loop()
+{
+    counter++;
+    lcd.setCursor(0, 1);
+    lcd.print(counter);
+}
